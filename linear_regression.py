933a1ac55969791d14bf4d952a7e21b9bcf94a3e
Chachu <sharadjoe48@gmail.com>
2018-02-16 23:38:05 +0530

    Completed the first tutorial with stock price eval

--- linear_regression.py
+++ linear_regression.py
@@ -1,35 +1,13 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+import quandl 
 
-def estimate_coef(x,y):
-    n=np.size(x)
-    mean_x=np.mean(x)
-    mean_y=np.mean(y)
-    cross_derivation=np.sum(y*x-n*mean_x*mean_y)
-    squared_derivation=np.sum(x**2-n*(mean_x**2))
-    slope=cross_derivation/squared_derivation
-    intercept=mean_y-slope*mean_x
-
-    return(intercept,slope)
+df=quandl.get('WIKI/GOOGL')
 
 
-def plotting(x,y,b):
-    plt.scatter(x,y,color="m",marker="o",s=30)
-    y_predicted=b[0]+b[1]*x
-    plt.plot(x, y_predicted, color = "g")
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+df=df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume', ]]
+df['HCL_PCT']=(df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100.0
+df['PCT_Change']=(df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100.0
 
+df= df[['Adj. Close','HCL_PCT','PCT_Change','Adj. Volume',]]
 
-x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
-b=estimate_coef(x,y)
-print(b[0])
-print(b[1])
-
-plotting(x,y,b)
-
-
- 
-
+print(df.head())
