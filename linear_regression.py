9dbaefb86564d167544dc1e3aa42a0ec6d3d1f47
Chachu <sharadjoe48@gmail.com>
2018-02-19 20:39:31 +0530

    Completed the algorithm

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl 
+import math
 
 df=quandl.get('WIKI/GOOGL')
 
@@ -9,5 +10,13 @@ df['HCL_PCT']=(df['Adj. High']-df['Adj. Close'])/df['Adj. Close']*100.0
 df['PCT_Change']=(df['Adj. Close']-df['Adj. Open'])/df['Adj. Open']*100.0
 
 df= df[['Adj. Close','HCL_PCT','PCT_Change','Adj. Volume',]]
+
+forecast= 'Adj.Close'
+df.fillna(-9999, inplace=True)
+
+forecast_out=int(math.ceil(0.01*len(df)))
+
+df['label']=df[forecast].shift(-forecast_out)
+df.dropna(inplace=True)
 
 print(df.head())
